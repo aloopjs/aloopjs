b9af19b881b2b897b032efdd0e96e6a812fc4233
Dev Prit <dev.prit@lcinocm.com>
2023-03-16 16:13:58 +0700

    Update reading modules and dev modules

--- index.js
+++ index.js
@@ -3,35 +3,59 @@ const basename = path.dirname(require.main.filename);
 global.App = global.App || {};
 global.App.base = {
   modulePath(el, sub){
-    // Check if have not start @
-    if (el.charAt(0) !== '@') root = [basename, 'src', el, sub].join(path.sep);
-    else root = [basename, 'node_modules', el.replace(/^@/, ''), sub].join(path.sep);
+    let dir = [];
+
+    if (el.root == 'node_modules') {
+      dir = [basename, 'node_modules', el.name];
+    } else {
+      dir = [basename, 'src', el.name];
+    }
+
+    if (sub) {
+      dir.push(sub);
+    }
   
-    return root;
+    return dir.join(path.sep);
   }
 };
 
 module.exports = {
-  rq(str){
-    // Check if have not start @
-    if (str.charAt(0) !== '@') {
-      return require([basename, 'src', str , 'index'].join(path.sep));
-    }
-
-    return require(str.replace(/^@/, ''));
+  rq(el){
+    let p = App.base.modulePath(el);
+    return require(p);
   },
 
   async run(appInfo, options) {
-    let args = {...appInfo, options};
+    let modules = [];
+
+    if (options.modules) {
+      modules = options.modules.map((item) => ({
+        name: item,
+        root: 'node_modules'
+      }));
+    }
+
+    if (options.devModules) {
+      let devModules = options.devModules.map((item) => ({
+        name: item,
+        root: 'src'
+      }));
+
+      modules = [...modules, ...devModules];
+    }
+
+    let args = {...appInfo, options: {
+      modules
+    }};
 
     // Before hook
-    options.modules.forEach((el) => {
+    modules.forEach((el) => {
       const module = this.rq(el);
       module.register && module.register(args);
     });
 
     // Boot hook
-    options.modules.forEach((el) => {
+    modules.forEach((el) => {
       const module = this.rq(el);
       module.boot && module.boot(args);
     });
